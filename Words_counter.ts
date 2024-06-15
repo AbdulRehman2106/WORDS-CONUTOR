e3cd83d234ddb22512bfd82f087eeba7b8f222b4
AbdulRehman2106 <ar2044612@gmail.com>
2024-06-15 23:36:48 +0500

    files added

---
+++ Words_counter.ts
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+
+import chalk, { Chalk } from "chalk";
+
+console.log(chalk.rgb(0,255,0)("\n\tWELCOME  TO  WORDS COUNTER\n\t"));
+
+// promte the user to enter a sentences
+
+let answers = await inquirer.prompt([
+
+    {
+
+    name : "sentence",
+
+    type : "input",
+
+    message : "Enter A Sentence\n\n",
+
+}
+
+]);
+
+// trimming the sentences and splitting it into words based on "spaces"
+
+let words = answers.sentence.trim( ).split("  ");
+
+// Analysis of user input sentences
+
+
+
+console.log(chalk.rgb(0,255,255)("\n- Sentence Words\n"));
+
+console.log(chalk.rgb(210,105,30)( words));
+
+console.log(chalk.rgb(255,255,0)`\n - Word Count : ${chalk.blueBright(words .length)}`);
+
+
+console.log(chalk.rgb(255,105,180)(`\n PRESENTING  BY  ABDUL  REHMAN\n`));
