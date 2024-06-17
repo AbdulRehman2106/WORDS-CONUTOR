9f467556f95d95a11feb345e792ff97208ea1788
AbdulRehman2106 <ar2044612@gmail.com>
2024-06-17 13:08:53 +0500

    changes

--- Words_counter.js
+++ Words_counter.js
@@ -1,13 +1,8 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const inquirer_1 = __importDefault(require("inquirer"));
-const chalk_1 = __importDefault(require("chalk"));
-console.log(chalk_1.default.rgb(0, 255, 0)("\n\tWELCOME  TO  WORDS COUNTER\n\t"));
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.rgb(0, 255, 0)("\n\tWELCOME  TO  WORDS COUNTER\n\t"));
 // promte the user to enter a sentences
-let answers = await inquirer_1.default.prompt([
+let answers = await inquirer.prompt([
     {
         name: "sentence",
         type: "input",
@@ -17,7 +12,7 @@ let answers = await inquirer_1.default.prompt([
 // trimming the sentences and splitting it into words based on "spaces"
 let words = answers.sentence.trim().split("  ");
 // Analysis of user input sentences
-console.log(chalk_1.default.rgb(0, 255, 255)("\n- Sentence Words\n"));
-console.log(chalk_1.default.rgb(210, 105, 30)(words));
-console.log(chalk_1.default.rgb(255, 255, 0) `\n - Word Count : ${chalk_1.default.blueBright(words.length)}`);
-console.log(chalk_1.default.rgb(255, 105, 180)(`\n PRESENTING  BY  ABDUL  REHMAN\n`));
+console.log(chalk.rgb(0, 255, 255)("\n- Sentence Words\n"));
+console.log(chalk.rgb(210, 105, 30)(words));
+console.log(chalk.rgb(255, 255, 0) `\n - Word Count : ${chalk.blueBright(words.length)}`);
+console.log(chalk.rgb(255, 105, 180)(`\n PRESENTING  BY  ABDUL  REHMAN\n`));
